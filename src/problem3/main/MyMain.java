fe2f720d524b228c6c2877562c458003e853cf15
divyamsethi <divyam.sethi_cs18@gla.ac.in>
2020-03-31 12:17:39 +0530

    all classes done

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,17 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+import problem3.myqueue.MyPriorityQueue;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("sagar",1);
+        obj.insert("divyam",4);
+        obj.insert("chirag",3);
+        obj.insert("deepu",2);
+        obj.insert("shubhi",5);
+        obj.display();
+    }
 }
